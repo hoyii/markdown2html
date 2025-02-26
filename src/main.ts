3d205fb71eb83182cc3087704275114a5e61574d
hoyii <812473076@qq.com>
2025-02-27 00:06:48 +0800

    删除主入口文件main.ts

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import MarkdownIt from '../lib/index.js'
-
-const mark = new MarkdownIt()
-
-console.log(mark.renderer.render(mark.parse('## Hello *world* !')))
-
-// console.log(mark.parse('Hello *world*!', {}))
