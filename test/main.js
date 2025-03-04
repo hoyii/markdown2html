cc6eeff5ac9901f2d112417310d951524bea0086
hoyii <812473076@qq.com>
2025-03-04 22:41:09 +0800

    删除测试文件 main.js

--- test/main.js
+++
@@ -1,7 +0,0 @@
-const Markdown2Html = require('../dist/cjs/index.js')
-
-const mark = new Markdown2Html()
-
-// console.log(mark.renderer.render(mark.parse('## Hello *world* !')))
-
-console.log(mark.render('![Picture](https://imzbf.github.io/md-editor-rt/imgs/mark_emoji.gif)'))
